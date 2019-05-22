3380a108b4780cc10961e8fa4efa1c8a0f8e7145
tzipisheinin <tzipi@audyx.com>
2019-05-22 18:47:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import domUtils from './DomUtils'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  utils;
+  
+  constructor() {
+    super();
+    this.utils = new domUtils();
+    this.state = {
+      value: ""
+    }
+  }
+
+  changeValueById = (id) => {
+    const val = this.utils.elementValue(id);
+    this.setState({
+      value: val
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div id="hello">hello world</div>
+        <button onClick={() => this.changeValueById("hello")}>
+          Click here to see the value
+        </button>
+        <span>The value is: {this.state.value}</span>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
